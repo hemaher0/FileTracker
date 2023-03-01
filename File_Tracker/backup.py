3dac066ef77bcc283cc31b60e7d19cb3cbed493b
hemaher0 <hemahero@naver.com>
2023-03-01 18:13:20 +0900

    version_1

--- File_Tracker/backup.py
+++ File_Tracker/backup.py
@@ -13,7 +13,11 @@ def backup_file(src_file, dest_dir):
 
     # Create backup filename based on current date and time
     now = datetime.datetime.now()
-    backup_file = os.path.join((dest_dir, f"{now.strftime('%Y-%m-%d_%H-%M-%S')}_{os.path.basename(src_file)}"))
+    backup_file = os.path.join(dest_dir, f"{now.strftime('%Y-%m-%d_%H-%M-%S')}_{os.path.basename(src_file)}")
 
     # Copy soure
     try:
+        shutil.copy(src_file, backup_file)
+        logging.info(f"Backup created: '{backup_file}'")
+    except Exception as e:
+        logging.error(f"Error creating backup: {str(e)}")
